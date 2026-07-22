c1272568f3ff3d336a6fae51738f9521561a9e28
GoldWitch Deploy <deploy@goldwitch.local>
2026-07-22 02:00:29 -0700

    Support Drakon Site Endpoint at /drakon and document Render env keys

--- routes/api/drakon.js
+++ routes/api/drakon.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var drakonControllers = require('@/controllers/api/drakon.js');
 
+// Drakon "Site Endpoint" is often https://yoursite.com/drakon (posts here)
+router.post('/', drakonControllers.webhook);
 router.post('/drakon_api', drakonControllers.webhook);
 
 module.exports = router;
